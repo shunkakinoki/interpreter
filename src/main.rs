4697e0e69fe2a547e594f91090bfd641c88052ee
Shun Kakinoki <shunkakinoki@gmail.com>
2022-12-25 14:04:41 -0800

    perf: ini cargo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+use anyhow::{Ok, Result};
+#[allow(unused_imports)]
+use bytes::Bytes;
+#[allow(unused_imports)]
+use ethers::{
+    abi::parse_abi,
+    prelude::BaseContract,
+    providers::{Http, Provider},
+};
+#[allow(unused_imports)]
+use revm::{
+    db::{CacheDB, EmptyDB, EthersDB},
+    Database, TransactOut, TransactTo, B160, EVM, U256 as rU256,
+};
+use std::{str::FromStr, sync::Arc};
+
+fn main() -> Result<()> {
     println!("Hello, world!");
+
+    let client = Provider::<Http>::try_from("https://mainnet.infura.io/v3/")?;
+    let client = Arc::new(client);
+
+    Ok(())
 }
